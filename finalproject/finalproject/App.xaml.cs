8aad657a9f89e14dec5b9304f07ef1febb41d461
yilmazayse00 <32266594+yilmazayse00@users.noreply.github.com>
2020-01-10 00:05:20 +0300

    Add files via upload

---
+++ finalproject/finalproject/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalproject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ finalproject/finalproject/TabbedPage1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace finalproject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Horror_Clicked(object sender, EventArgs e)
+        {
+            //TapGestureRecognizer.NumberOfTapsRequiredProperty.Equals(2);
+            await Navigation.PushAsync(new HorrorListPage());
+        }
+
+    }
+}
